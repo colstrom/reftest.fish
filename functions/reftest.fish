8b1e22a66aebe8e5ba12b511af5b6279b3806323
Chris Olstrom <chris@olstrom.com>
2016-05-09 10:27:25 -0700

    Runs tests against reference implementations

---
+++ functions/reftest.fish
@@ -0,0 +1,32 @@
+function reftest --argument path
+    switch "$path"
+        case -- --help -h
+            echo 'usage: reftest <path>' >&2
+            false
+        case ''
+            reftest (fish_realpath .)
+        case '*'
+            use (fish_realpath $path/functions)
+            set failed 0
+            for function in $path/functions/*.fish
+                set unit (basename -s .fish $function)
+                if test -f reference/$unit
+                    for case in test/cases/*
+                        set test (cat $case)
+                        set scenario (string replace --all --regex '[-_]' ' ' (basename $case))
+
+                        if test (eval $unit $test) = (eval reference/$unit $test)
+                            echo ok - $scenario
+                        else
+                            echo not ok - $unit $scenario
+                            set failed 1
+                            diff (eval $unit $test | psub) (eval reference/$unit $test | psub) | string replace --regex '^' '#  '
+                        end
+                    end
+                else
+                    echo not ok - $unit has no reference implementation
+                    set failed 1
+                end
+            end
+    end
+end
